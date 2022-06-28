4d293a4bb07f82795260117bf4de0ed9a3f98d15
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-06-28 23:50:06 +0530

    Add files via upload

---
+++ program163.cpp
@@ -0,0 +1,26 @@
+#include"Header162.h";
+
+DoublyCLL::DoublyCLL()
+{}
+
+DoublyCLL::InsertFirst(int iNo)
+{}
+
+DoublyCLL::InsertLast(int iNo)
+{}
+
+DoublyCLL::InsertAtPos(int iNo, int iPos)
+{}
+
+DoublyCLL::DeleteFirst()
+{}
+
+DoublyCLL::DeleteLast()
+{}
+
+DoublyCLL::DeleteAtPos(int iPos)
+{}
+
+void DoublyCLL::Display()
+{}
+
